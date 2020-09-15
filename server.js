0f50fab00d04c8cbb1ca71bd10209a0cb4a2de30
Dylan <dylanlarrivee@gmail.com>
2020-09-15 15:52:22 -0700

    removed NODE_ENV references

--- server.js
+++ server.js
@@ -24,19 +24,6 @@ app.use(bodyParser.json());
 
 app.use("/api", deliveryRoutes);
 
-
-console.log("running in ", process.env.NODE_ENV)
-
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-   // res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 let server = app.listen(PORT, function () {
   console.log(`Server is starting at ${PORT}`);
 });
